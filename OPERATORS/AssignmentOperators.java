b34dfa173e9a61edb244fdb54204148319ef3c5f
Sachinbarathi <sachinbarathi.p@gmail.com>
2026-08-21 16:29:01 +0530

    Add files via upload

---
+++ OPERATORS/AssignmentOperators.java
@@ -0,0 +1,17 @@
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int n = 10;
+
+        n += 5;
+        System.out.println("+= " + n);
+
+        n -= 3;
+        System.out.println("-= " + n);
+
+        n *= 2;
+        System.out.println("*= " + n);
+
+        n /= 4;
+        System.out.println("/= " + n);
+    }
+}
---
+++ OPERATORS/TernaryOperator.java
@@ -0,0 +1,9 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 20;
+
+        int largest = (a > b) ? a : b;
+        System.out.println("Largest: " + largest);
+    }
+}
